0103c609c800a0591224fd514e503bafb39c2cf8
Rodrigo Azevedo <rodrigowbazevedo@yahoo.com.br>
2014-04-12 22:27:21 -0300

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,156 @@
-simpleUpload Jquery Ajax upload plugin
-======================================
+simpleUpload Jquery Ajax upload
+====================================
+
+simpleUpload é um simples Jquery plugin para facilitar o envio de arquivos
+sem depender de um formulário.
+
+## Usage
+
+#### Html
+
+```html
+<input type="file" name="arquivo" id="simpleUpload" multiple >
+<button type="button" id="enviar">Enviar</button>
+```
+
+#### Javascript
+
+```javascript
+$('#simpleUpload').simpleUpload({
+	url: 'upload.php',
+	trigger: '#enviar',
+	success: function(data){
+		alert('Envio com sucesso');
+	}
+});
+```
+
+#### PHP
+
+```php
+	$arquivo = $_FILES['arquivo'];
+```
+
+Se o parâmetro `trigger` não for definido o envio será iniciado
+no evento `change` do elemento.
+
+### Data atribute
+Todos os parâmetros podem ser enviados via atributo
+
+```html
+<input type="file" name="arquivo_1" class="upload" data-trigger="#enviar1" data-url="upload1.php" multiple >
+<button type="button" id="enviar1">Enviar 1</button>
+
+<input type="file" name="arquivo_2" class="upload" data-trigger="#enviar1" data-url="upload2.php" multiple >
+<button type="button" id="enviar2">Enviar 2</button>
+```
+
+```javascript
+$('.upload').simpleUpload();
+```
+
+### On file change
+
+Callback no evento `change` do elemento com retorno dos metadados
+do arquivo
+
+```javascript
+$('#simpleUpload').simpleUpload({
+	url: 'upload.php',
+	change: function(files){
+		$.each(files, function(i, file){
+			alert('Nome: '+file.name+' - Tamanho: '+file.size+' - Tipo: '+file.type);
+		});
+	}
+});
+```
+
+### Valores extras
+
+O parâmetro `fields` recebe um json com campos a serem enviados ao Backend
+
+```javascript
+$('#simpleUpload').simpleUpload({
+	fields: {
+		valor : 'Valor 1',
+		array : ['array 1', 'array 2']
+	}
+});
+```
+
+```php
+	$arquivo = $_FILES['arquivo'];
+	$valor = $_POST['valor'];
+	$array = $_POST['array'];
+
+	echo $valor;
+	echo $array[0];
+	echo $array[1];
+```
+
+### Arquivos permitidos
+
+O parâmetro `types` recebe a lista de extensões permitidas em caso
+de arquivo inválido retorna o método `error` com o parametro `erro`
+e o atributo `type` com o valor 'fileType'
+
+```javascript
+$('#simpleUpload').simpleUpload({
+	types: ['jpg', 'png', 'pdf'],
+	error: function(erro){
+		if(error.type == 'fileType') alert('Arquivo inválido.');
+	}
+});
+```
+
+### Tamnho permitido
+O tamanho máximo por arquivo por padrão é 5mb para mudar
+use o parâmetro `size` o valor deve ser passado em kb
+
+
+```javascript
+$('#simpleUpload').simpleUpload({
+	size: 3072, // 3 mb
+	types: ['jpg', 'png', 'pdf'],
+	error: function(erro){
+		if(error.type == 'size'){
+			alert('Tamanho não permitido.');
+		}else if(error.type == 'fileType'){
+			alert('Arquivo inválido.');
+		}else{
+			...
+		}
+	}
+});
+```
+
+O método `error` chamado para arquivos inválidos fora o tamanho e para erro no retorno do Ajax
+
+
+### Outros parâmetros
+
+O simpleUpload suporta a maioria dos parâmetros que o $.ajax() suporta.
+
+Os parâmetros são:
+-url ( string: Caminho do arquivo Backend )
+-change ( function: método onChange do input file  )
+-types ( array: lista de extensões permitidas )
+-size ( int: tamanho permitido em kb )
+-fields ( json: campos extra a serem enviados aceita array )
+-error ( function: retorno de erro )
+-success ( function: retorno de sucesso )
+-beforeSend ( function: executado antes de enviar )
+-async
+-global
+-dataType
+-contents
+-jsonp
+-jsonpCallback
+-password
+-username
+-statusCode
+
+### Autor
+- Email: rodrigowbazevedo@yahoo.com.br
+- GitHub: https://github.com/rodrigowbazevedo
+- Work: Programmer on  @ http://kmdois.com.br
--- simpleUpload.js
+++ simpleUpload.js
@@ -1,109 +1,116 @@
-$.fn.simpleUpload = function(options){
+(function($){
+	$.fn.simpleUpload = function(options){
 
-	var def = {
-		url          : null,
-		change       : null,
-		error        : null,
-		types        : null,
-		fields       : null,
-		size         : 5242880
-	};
-
-	return this.each(function(){
-
-		var $this = $(this);
-
-		$.each(this.attributes, function(){
-			if(this.specified) {
-				if(this.name.indexOf('data-') > -1){
-					var name = this.name.replace('data-','')
-					options[name] = this.value;
-				}
-			}
-		});
-
-		var params = $.extend({}, def, options);
-
-		if(typeof params.change == 'function') $this.change(params.change);
-
-		if(params.trigger){
-			$(params.trigger).click( send );
-		}else{
-			$this.change( send );
-		}
-
-		function send(){
-			var name = $this.attr('name');
-
-			var data = new FormData();
-
-			for(var i in $this[0].files){
-				if(typeof $this[0].files[i] == 'object'){
-
-					var file = $this[0].files[i];
-					if(!typeCheck(file.name)){
-						if(typeof params.error == 'function') params.error({ type: 'fileType', message: 'Arquivo inválido.' });
-						return false;
-					}
-
-					if(file.size > params.size){
-						if(typeof params.error == 'function') params.error({ type:'size', message:'Tamanho maior que o permitido.' });
-						return false;
-
-					}
-
-					data.append(name+'['+i+']', file);
-
-				}
-			}
-
-			if(typeof params.fields == 'object'){
-				for(var i in params.fields){
-					data.append(i, params.fields[i]);
-				}
-			}
-
-			var ajax = {
-				url         : params.url,
-				data        : data,
-				cache       : false,
-				contentType : false,
-				processData : false,
-				type        : 'POST',
-				complete: function(){
-					$this.val('');
-					if(params.complete) params.complete(arguments);
-				}
-			}
-
-			if(params.error != null ) ajax.error         = params.error;
-			if(params.success)        ajax.success       = params.success;
-			if(params.async)          ajax.async         = params.async;
-			if(params.beforeSubmit)   ajax.beforeSubmit  = params.beforeSubmit;
-			if(params.global)         ajax.global        = params.global;
-			if(params.dataType)       ajax.dataType      = params.dataType;
-			if(params.contents)       ajax.contents      = params.contents;
-			if(params.jsonp)          ajax.jsonp         = params.jsonp;
-			if(params.jsonpCallback)  ajax.jsonpCallback = params.jsonpCallback;
-			if(params.password)       ajax.password      = params.password;
-			if(params.username)       ajax.username      = params.username;
-			if(params.statusCode)     ajax.statusCode    = params.statusCode;
-
-			$.ajax(ajax);
+		var def = {
+			url          : null,
+			change       : null,
+			error        : null,
+			types        : null,
+			fields       : null,
+			size         : 5120
 		};
 
-		function typeCheck(name){
-			if($.isArray(params.types)){
-				if(params.types.indexOf($.getExt(name)) > -1) return true;
-				return false;
+		return this.each(function(){
+
+			var $this = $(this);
+			if(!options) options = {};
+
+			$.each(this.attributes, function(){
+				if(this.specified) {
+					if(this.name.indexOf('data-') > -1){
+						var name = this.name.replace('data-','')
+						options[name] = (typeof window[this.value] == 'function')? eval(this.value) : this.value;
+					}
+				}
+			});
+
+			var params = $.extend({}, def, options);
+
+			if(typeof params.change == 'function') $this.change(function(){
+				params.change($this[0].files);
+			});
+
+			if(params.trigger){
+				$(params.trigger).click( send );
+			}else{
+				$this.change( send );
 			}
-			return true;
+
+			function send(){
+				var name = $this.attr('name'),
+					data = new FormData();
+
+				for(var i in $this[0].files){
+					if(typeof $this[0].files[i] == 'object'){
+
+						var file = $this[0].files[i];
+						if(!typeCheck(file.name)){
+							if(typeof params.error == 'function') params.error({ type: 'fileType' });
+							return false;
+						}
+
+						if((file.size/params.size) > params.size){
+							if(typeof params.error == 'function') params.error({ type:'size' });
+							return false;
+						}
+
+						data.append(name+'['+i+']', file);
+					}
+				}
+
+				if(typeof params.fields == 'object'){
+					for(var i in params.fields){
+						if($.isArray(params.fields[i])){
+							for(var k in params.fields[i]) data.append(i+'['+k+']', params.fields[i][k]);
+						}else{
+							data.append(i, params.fields[i]);
+						}
+					}
+				}
+
+				var ajax = {
+					url         : params.url,
+					data        : data,
+					cache       : false,
+					contentType : false,
+					processData : false,
+					type        : 'POST',
+					complete: function(){
+						$this.val('');
+						if(params.complete) params.complete(arguments);
+					}
+				}
+
+				if(params.error != null ) ajax.error         = params.error;
+				if(params.success)        ajax.success       = params.success;
+				if(params.async)          ajax.async         = params.async;
+				if(params.beforeSend)     ajax.beforeSend    = params.beforeSend;
+				if(params.global)         ajax.global        = params.global;
+				if(params.dataType)       ajax.dataType      = params.dataType;
+				if(params.contents)       ajax.contents      = params.contents;
+				if(params.jsonp)          ajax.jsonp         = params.jsonp;
+				if(params.jsonpCallback)  ajax.jsonpCallback = params.jsonpCallback;
+				if(params.password)       ajax.password      = params.password;
+				if(params.username)       ajax.username      = params.username;
+				if(params.statusCode)     ajax.statusCode    = params.statusCode;
+
+				$.ajax(ajax);
+			};
+
+			function typeCheck(name){
+				if($.isArray(params.types)){
+					if(params.types.indexOf($.getExt(name)) > -1) return true;
+					return false;
+				}
+				return true;
+			}
+		});
+	};
+
+	$.extend(true,{
+		getExt: function(name){
+			return name.substr(name.lastIndexOf(".") + 1);
 		}
 	});
-};
 
-$.extend(true,{
-	getExt: function(name){
-		return name.substr(name.lastIndexOf(".") + 1);
-	}
-});
+})(jQuery);
